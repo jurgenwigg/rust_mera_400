f5d43f6ee6d7881bd6af9105de49f08b5b6da265
jurgenwigg <jurgenwigg@protonmail.com>
2023-01-06 18:12:38 +0100

    basic cpu implementation - part I

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,33 @@ extern crate rust_mera_400;
 
 use rust_mera_400::registers::simple_register::{UniversalRegister, Register};
 
-struct CpuRegisters {
-    r0:UniversalRegister,
-    r1:UniversalRegister,
-    r2:UniversalRegister,
-    r3:UniversalRegister,
-    r4:UniversalRegister,
-    r5:UniversalRegister,
-    r6:UniversalRegister,
-    r7:UniversalRegister,
+pub struct Cpu {
+    pub reg_ac:UniversalRegister,
+    pub reg_r0:UniversalRegister,
+    pub reg_ic:UniversalRegister
 }
 
+impl Cpu {
+    fn new()-> Self {
+        Cpu {
+            reg_ac: Register::new(17), // accumalor register
+            reg_r0: Register::new(16), // R0 register
+            reg_ic: Register::new(16)  // IC register
+        }
+    }
+
+    pub fn interpret(&mut self, program: Vec<u8>) {
+        loop {
+            let opscode = program[self.reg_ic as usize];
+            self.reg_ic += 1;
+
+            match opscode {
+                _ => todo!()
+            }
+        }
+    }
+}
 
 fn main() {
-    let mut cpu= CpuRegisters{
-        r0:Register::new(16),
-        r1:Register::new(16),
-        r2:Register::new(16),
-        r3:Register::new(16),
-        r4:Register::new(16),
-        r5:Register::new(16),
-        r6:Register::new(16),
-        r7:Register::new(16),
-    };
-    let mut example_register:UniversalRegister = Register::new(8);
-    println!("{}",example_register.state());
-    example_register.set_bit(0,true);
-    println!("{}",example_register.state());
-    cpu.r0.set_bit(13,true);
-    cpu.r1.set_bit(12,true);
-    println!("{}",cpu.r0.state());
-    println!("{}",cpu.r1.state());
+
 }
--- src/registers/simple_register.rs
+++ src/registers/simple_register.rs
@@ -2,24 +2,32 @@ pub struct UniversalRegister { register:Vec<bool> }
 
 pub trait Register {
     fn new(size: usize) -> Self;
+    fn set_bit(&mut self, bit_position:usize, value:bool);
+    fn state(&self) -> String;
+    fn size(&self) -> usize;
 }
-impl UniversalRegister {
-    pub fn len(&mut self) -> usize{
+
+impl Register for UniversalRegister {
+    fn new(size:usize) -> UniversalRegister {
+        UniversalRegister{ register : vec![false;size] }
+    }
+    fn size(&self) -> usize{
         self.register.len()
     }
 
-    pub fn set_bit(&mut self, bit_position:usize, value:bool) {
+    fn set_bit(&mut self, bit_position:usize, value:bool) {
         self.register[bit_position] = value;
     }
 
-    pub fn state(&mut self) -> String {
+    fn state(&self) -> String {
         let reg = &self.register;
         let temp = reg.into_iter().map(|elem| *elem as i8).into_iter();
         String::from_iter(temp.map(|elem| elem.to_string()))
    }
 }
-impl Register for UniversalRegister {
-    fn new(size:usize) -> UniversalRegister {
-        UniversalRegister{ register : vec![false;size] }
+
+impl From<usize> for UniversalRegister {
+    fn from(item: i32) -> Self {
+        Number { value: item }
     }
 }
