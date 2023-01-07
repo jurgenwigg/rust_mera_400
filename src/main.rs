28ce84261914caddef13b448c3e857f37fdcac47
jurgenwigg <jurgenwigg@protonmail.com>
2023-01-07 19:11:30 +0100

    formatted .rs files in src/

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ extern crate rust_mera_400;
 // use rust_mera_400::registers::simple_register::{UniversalRegister, Register};
 
 pub struct Cpu {
-    pub reg_r0:u16,
-    pub reg_ac:u16, // 17bit width in real machine
-    pub reg_ic:u16,
-    pub reg_x:u16 //register x?
+    pub reg_r0: u16,
+    pub reg_ac: u16, // 17bit width in real machine
+    pub reg_ic: u16,
+    pub reg_x: u16, //register x?
 }
 
 impl Cpu {
@@ -15,7 +15,7 @@ impl Cpu {
             reg_r0: 0,
             reg_ac: 0,
             reg_ic: 0,
-            reg_x:0
+            reg_x: 0,
         }
     }
 
@@ -23,51 +23,50 @@ impl Cpu {
         self.reg_ac = value;
         self.update_zero_and_negative_flags(self.reg_ac);
     }
-  
+
     fn tax(&mut self) {
         self.reg_x = self.reg_ac;
         self.update_zero_and_negative_flags(self.reg_x);
     }
-   
-     fn update_zero_and_negative_flags(&mut self, result: u16) {
-         if result == 0 {
-             self.reg_r0 = self.reg_r0 | 0b0000_0010;
-         } else {
-             self.reg_r0 = self.reg_r0 & 0b1111_1101;
-         }
- 
-         if result & 0b1000_0000 != 0 {
-             self.reg_r0 = self.reg_r0 | 0b1000_0000;
-         } else {
-             self.reg_r0 = self.reg_r0 & 0b0111_1111;
-         }
-     }
+
+    fn update_zero_and_negative_flags(&mut self, result: u16) {
+        if result == 0 {
+            self.reg_r0 = self.reg_r0 | 0b0000_0010;
+        } else {
+            self.reg_r0 = self.reg_r0 & 0b1111_1101;
+        }
+
+        if result & 0b1000_0000 != 0 {
+            self.reg_r0 = self.reg_r0 | 0b1000_0000;
+        } else {
+            self.reg_r0 = self.reg_r0 & 0b0111_1111;
+        }
+    }
 
     pub fn interpret(&mut self, program: Vec<u16>) {
         self.reg_ic = 0;
-    
+
         loop {
             let opscode = program[self.reg_ic as usize];
             self.reg_ic += 1;
-    
+
             match opscode {
                 0xA9 => {
                     let param = program[self.reg_ic as usize];
                     self.reg_ic += 1;
-                    
+
                     self.lda(param);
                 }
-    
-                0xAA => self.tax(),
-    
-                0x00 => return,
-                
-                _ => todo!(),
 
+                0xAA => self.tax(),
+
+                0x00 => return,
+
+                _ => todo!(),
             }
         }
     }
- }
+}
 
 fn main() {
     println!("Mera400 CPU");
