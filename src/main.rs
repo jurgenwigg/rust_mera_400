64ab4dc067021c1c9226afa931a654c0f9e8254f
jurgenwigg <jurgenwigg@protonmail.com>
2023-01-07 19:07:50 +0100

    new approach is compiling

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,74 @@
 extern crate rust_mera_400;
 
-use rust_mera_400::registers::simple_register::{UniversalRegister, Register};
+// use rust_mera_400::registers::simple_register::{UniversalRegister, Register};
 
-struct CpuRegisters {
-    r0:UniversalRegister,
-    r1:UniversalRegister,
-    r2:UniversalRegister,
-    r3:UniversalRegister,
-    r4:UniversalRegister,
-    r5:UniversalRegister,
-    r6:UniversalRegister,
-    r7:UniversalRegister,
+pub struct Cpu {
+    pub reg_r0:u16,
+    pub reg_ac:u16, // 17bit width in real machine
+    pub reg_ic:u16,
+    pub reg_x:u16 //register x?
 }
 
+impl Cpu {
+    pub fn new() -> Self {
+        Cpu {
+            reg_r0: 0,
+            reg_ac: 0,
+            reg_ic: 0,
+            reg_x:0
+        }
+    }
+
+    fn lda(&mut self, value: u16) {
+        self.reg_ac = value;
+        self.update_zero_and_negative_flags(self.reg_ac);
+    }
+  
+    fn tax(&mut self) {
+        self.reg_x = self.reg_ac;
+        self.update_zero_and_negative_flags(self.reg_x);
+    }
+   
+     fn update_zero_and_negative_flags(&mut self, result: u16) {
+         if result == 0 {
+             self.reg_r0 = self.reg_r0 | 0b0000_0010;
+         } else {
+             self.reg_r0 = self.reg_r0 & 0b1111_1101;
+         }
+ 
+         if result & 0b1000_0000 != 0 {
+             self.reg_r0 = self.reg_r0 | 0b1000_0000;
+         } else {
+             self.reg_r0 = self.reg_r0 & 0b0111_1111;
+         }
+     }
+
+    pub fn interpret(&mut self, program: Vec<u16>) {
+        self.reg_ic = 0;
+    
+        loop {
+            let opscode = program[self.reg_ic as usize];
+            self.reg_ic += 1;
+    
+            match opscode {
+                0xA9 => {
+                    let param = program[self.reg_ic as usize];
+                    self.reg_ic += 1;
+                    
+                    self.lda(param);
+                }
+    
+                0xAA => self.tax(),
+    
+                0x00 => return,
+                
+                _ => todo!(),
+
+            }
+        }
+    }
+ }
 
 fn main() {
-    let mut cpu= CpuRegisters{
-        r0:Register::new(16),
-        r1:Register::new(16),
-        r2:Register::new(16),
-        r3:Register::new(16),
-        r4:Register::new(16),
-        r5:Register::new(16),
-        r6:Register::new(16),
-        r7:Register::new(16),
-    };
-    let mut example_register:UniversalRegister = Register::new(8);
-    println!("{}",example_register.state());
-    example_register.set_bit(0,true);
-    println!("{}",example_register.state());
-    cpu.r0.set_bit(13,true);
-    cpu.r1.set_bit(12,true);
-    println!("{}",cpu.r0.state());
-    println!("{}",cpu.r1.state());
+    println!("Mera400 CPU");
 }
