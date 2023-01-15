c6f1ba838bc7a32634df6793558c0922494728a6
jurgenwigg <jurgenwigg@protonmail.com>
2023-01-15 21:00:57 +0100

    refactor with binary_layout crate

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,11 @@
 extern crate rust_mera_400;
-
-use rust_mera_400::registers::simple_register::{UniversalRegister, Register};
-
-struct CpuRegisters {
-    r0:UniversalRegister,
-    r1:UniversalRegister,
-    r2:UniversalRegister,
-    r3:UniversalRegister,
-    r4:UniversalRegister,
-    r5:UniversalRegister,
-    r6:UniversalRegister,
-    r7:UniversalRegister,
-}
-
+use rust_mera_400::registers::universal::universal_register;
 
 fn main() {
-    let mut cpu= CpuRegisters{
-        r0:Register::new(16),
-        r1:Register::new(16),
-        r2:Register::new(16),
-        r3:Register::new(16),
-        r4:Register::new(16),
-        r5:Register::new(16),
-        r6:Register::new(16),
-        r7:Register::new(16),
-    };
-    let mut example_register:UniversalRegister = Register::new(8);
-    println!("{}",example_register.state());
-    example_register.set_bit(0,true);
-    println!("{}",example_register.state());
-    cpu.r0.set_bit(13,true);
-    cpu.r1.set_bit(12,true);
-    println!("{}",cpu.r0.state());
-    println!("{}",cpu.r1.state());
+    // packet_data: &mut [u8]
+    let mut first_register = universal_register::View::new(0b0001000010000100_u16.to_be_bytes());
+    println!("{:b}", first_register.data().read());
+    let reg_value = first_register.data().read();
+    first_register.data_mut().write(reg_value+0b0110);
+    println!("{:b}", first_register.data().read());
 }
--- src/registers/mod.rs
+++ src/registers/mod.rs
@@ -1,1 +1,2 @@
 pub mod simple_register;
+pub mod universal;
